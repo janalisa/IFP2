627c9cb62532999906c2ce744c2e957a4d6c7356
Leonie <l.freysommer@gmail.com>
2018-06-28 16:39:24 +0200

    #include<KrassenCode>

--- vector.h
+++ vector.h
@@ -5,4 +5,25 @@
 #ifndef IFP_VECTOR_H
 #define IFP_VECTOR_H
 
+class vector{
+    double a,b,c;
+
+public:
+    vector();
+    vector(double x, double y, double z);
+    ~vector();
+
+void setx(double a);
+void sety(double b);
+void setz(double c);
+
+double phi();
+double theta();
+double  len();
+
+};
+
+
 #endif //IFP_VECTOR_H
+
+
